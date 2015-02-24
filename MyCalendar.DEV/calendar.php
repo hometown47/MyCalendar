be78b82bca879512a81d0367fbe5db35c0f12378
36066Pe <36066Pe@YEPS91336.YellGlobal.net>
2015-02-24 11:12:55 +0000

    Correct typo in comment

--- MyCalendar.DEV/calendar.php
+++ MyCalendar.DEV/calendar.php
@@ -1,6 +1,6 @@
 <?php
 
-// Pete Jones February Makes some Changes in the barnch
+// Pete Jones February Makes some Changes in the branch
 
 header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 header("Cache-Control: post-check=0, pre-check=0", false);
